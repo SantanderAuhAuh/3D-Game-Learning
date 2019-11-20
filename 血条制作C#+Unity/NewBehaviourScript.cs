fd1e234d75da6df1f6aa52f222d7016ce1b28c0a
e <Santander2019@163.com>
2019-11-20 22:30:05 +0800

    add

---
+++ 血条制作C#+Unity/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float value;
+
+    private float temp;
+
+    void Start()
+    {
+        value = 0.0f;
+        temp = 0.0f;
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(250, 20, 40, 20), "+"))
+        {
+            if(temp<10)
+                temp++;
+        }
+
+        if (GUI.Button(new Rect(300, 20, 40, 20), "-"))
+        {
+            if(temp>0)
+                temp--;
+        }
+
+        value = Mathf.Lerp(value, temp, 0.01f);
+
+        GUI.color = Color.red;
+        GUI.HorizontalScrollbar(new Rect(20, 20, 200, 20), 0.0f, value, 0.0f, 10.0f);
+    }
+}
---
+++ 血条制作C#+Unity/NewBehaviourScript1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public GameObject game_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.LookAt(game_object.transform);
+    }
+}
