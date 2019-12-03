9903422393c020545f52fe304974d4fc5fe1b58f
e <Santander2019@163.com>
2019-12-03 16:45:32 +0800

    add

---
+++ AR 小游戏/AR Game/Assets/Scripts/Acceleration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acceleration : MonoBehaviour
+{
+    public float speed = 200;
+
+    static bool isTouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTouch = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount >= 1)
+        {
+            isTouch = true;
+        }
+        if (isTouch)
+        {
+            Vector3 move = new Vector3(Input.acceleration.x * speed * Time.deltaTime, Input.acceleration.y * Time.deltaTime);
+            transform.Translate(move);
+        }
+    }
+}
---
+++ AR 小游戏/AR Game/Assets/Scripts/ButtonBack_click.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonBack_click : MonoBehaviour
+{
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        // 绑定监听器
+        btn.onClick.AddListener(TaskOnclick);
+    }
+
+    void TaskOnclick()
+    {
+        // 加载Build Settings中的索引对应的Scene
+        Application.LoadLevel(0);
+    }
+}
